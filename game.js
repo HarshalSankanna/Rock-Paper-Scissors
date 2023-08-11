8c26546396e16dbceed5e58dc4753f1b39613efa
Harshal <harshal0921@gmail.com>
2023-08-11 22:17:58 +0530

    Create html and javascript files with initial code

---
+++ game.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+  let arr = ["rock", "paper", "scissors"];
+  let Choice = arr[Math.floor(Math.random() * arr.length)];
+  return Choice;
+}
+
+function playRound(playerSelection, computerSelection) {}
