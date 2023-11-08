605cb3e19b10655c6d8bcde5345c6c423274b96a
Harshal <harshal0921@gmail.com>
2023-11-08 19:22:16 +0530

    Created a fucntion that plays a single round.

--- game.js
+++ game.js
@@ -4,4 +4,32 @@ function getComputerChoice() {
   return Choice;
 }
 
-function playRound(playerSelection, computerSelection) {}
+// console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase();
+  let computer = computerSelection;
+  let result = "tie";
+  if (player == "rock" && computer == "scissors") {
+    result = "You Win! Rock beats Scissors";
+  } else if (player == "rock" && computer == "paper") {
+    result = "You Lose! Paper beats Rock";
+  } else if (player == "rock" && computer == "rock") {
+    result = "It's a tie!";
+  } else if (player == "paper" && computer == "scissors") {
+    result = "You Lose! Scissors beat Paper";
+  } else if (player == "paper" && computer == "paper") {
+    result = "It's a tie!";
+  } else if (player == "paper" && computer == "rock") {
+    result = "You Win! Paper beats Rock";
+  } else if (player == "scissors" && computer == "rock") {
+    result = "You Lose! Rock beats Scissors";
+  } else if (player == "scissors" && computer == "paper") {
+    result = "You Win! Scissors beat paper";
+  } else if (player == "scissors" && computer == "scissors") {
+    result = "It's a tie!";
+  } else {
+    result = "Invalid choice";
+  }
+  return result;
+}
