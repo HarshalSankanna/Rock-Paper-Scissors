3e43afb95b2a6b834698b7e76f1b5aae3408cf2d
Harshal <harshal0921@gmail.com>
2023-12-14 18:58:07 +0530

    added ui to the game

--- game.js
+++ game.js
@@ -4,67 +4,118 @@ function getComputerChoice() {
   return Choice;
 }
 
+// game logic
 function playRound(playerSelection, computerSelection) {
   let player = playerSelection.toLowerCase();
   let computer = computerSelection;
   let result = 0;
-  if (player == "rock" && computer == "scissors") {
-    result = 1;
-  } else if (player == "rock" && computer == "paper") {
-    result = 2;
-  } else if (player == "rock" && computer == "rock") {
-    result = 0;
-  } else if (player == "paper" && computer == "scissors") {
-    result = 2;
-  } else if (player == "paper" && computer == "paper") {
-    result = 0;
-  } else if (player == "paper" && computer == "rock") {
-    result = 1;
-  } else if (player == "scissors" && computer == "rock") {
-    result = 2;
-  } else if (player == "scissors" && computer == "paper") {
-    result = 1;
-  } else if (player == "scissors" && computer == "scissors") {
-    result = 0;
+  if (
+    (player == "rock" && computer == "scissors") ||
+    (player == "paper" && computer == "rock") ||
+    (player == "scissors" && computer == "paper")
+  ) {
+    result = 1; // player wins
+  } else if (
+    (player == "rock" && computer == "paper") ||
+    (player == "paper" && computer == "scissors") ||
+    (player == "scissors" && computer == "rock")
+  ) {
+    result = 2; // computer wins
   } else {
-    result = 5;
+    result = 0; // tie
   }
+
   return result;
 }
 
-function game(computerSelection) {
-  let computer = computerSelection;
-  let player = prompt("Rock, Paper or Scissors?");
-  let resultCode = playRound(player, computer);
-  if (resultCode == 1) {
-    alert("You win!");
-    return resultCode;
-  } else if (resultCode == 2) {
-    alert("You lose!");
-    return resultCode;
-  } else if (resultCode == 0) {
-    alert("It's a tie!");
-  } else {
-    alert("Invalid choice");
-  }
-}
-
+let playerScore = document.querySelector("#player");
+let computerScore = document.querySelector("#computer");
 let playerCount = 0;
 let computerCount = 0;
+let result = document.querySelector("#result");
 
-while (playerCount < 3 && computerCount < 3) {
-  let key = game(getComputerChoice());
-  if (key == 1) {
+function rockClicked() {
+  let computer = getComputerChoice();
+  let player = "rock";
+  let resultCode = playRound(player, computer);
+  // used setTimeout method to call functions
+  // if has arguments then use anonymous arrow function
+  updateScore(resultCode);
+  setTimeout(match, 3000);
+}
+
+function paperClicked() {
+  let computer = getComputerChoice();
+  let player = "paper";
+  let resultCode = playRound(player, computer);
+  updateScore(resultCode);
+  setTimeout(match, 3000);
+}
+
+function scissorsClicked() {
+  let computer = getComputerChoice();
+  let player = "scissors";
+  let resultCode = playRound(player, computer);
+  updateScore(resultCode);
+  setTimeout(match, 3000);
+}
+
+// update score
+function updateScore(resultCode) {
+  if (resultCode == 1) {
     playerCount++;
-  } else if (key == 2) {
+    playerScore.textContent = playerCount.toString();
+    computerScore.textContent = computerCount.toString();
+    result.textContent = "You win!";
+  } else if (resultCode == 2) {
     computerCount++;
+    playerScore.textContent = playerCount.toString();
+    computerScore.textContent = computerCount.toString();
+    result.textContent = "You lost!";
+  } else {
+    playerScore.textContent = playerCount.toString();
+    computerScore.textContent = computerCount.toString();
+    result.textContent = "Round tied!";
   }
 }
 
-if (playerCount > computerCount) {
-  alert("You win the series! " + playerCount + "-" + computerCount);
-} else if (computerCount > playerCount) {
-  alert("You lose the series! " + playerCount + "-" + computerCount);
-} else {
-  alert("Series tied!");
+function gameStart() {
+  let Rock = document.querySelector("#rock");
+  let Paper = document.querySelector("#paper");
+  let Scissors = document.querySelector("#scissors");
+
+  // to remove previous event listeners if they exist
+  Rock.removeEventListener("click", rockClicked);
+  Paper.removeEventListener("click", paperClicked);
+  Scissors.removeEventListener("click", scissorsClicked);
+
+  // to add new event listeners
+  Rock.addEventListener("click", rockClicked);
+  Paper.addEventListener("click", paperClicked);
+  Scissors.addEventListener("click", scissorsClicked);
 }
+
+// to declare winner
+function match() {
+  if (playerCount == 5) {
+    alert("You win the match!");
+    // reset scores
+    playerCount = 0;
+    computerCount = 0;
+    playerScore.textContent = playerCount.toString();
+    computerScore.textContent = computerCount.toString();
+    result.textContent = "-";
+    gameStart();
+  } else if (computerCount == 5) {
+    alert("Computer won the match!");
+    // reset scores
+    playerCount = 0;
+    computerCount = 0;
+    playerScore.textContent = playerCount.toString();
+    computerScore.textContent = computerCount.toString();
+    result.textContent = "-";
+    gameStart();
+  }
+}
+
+gameStart();
